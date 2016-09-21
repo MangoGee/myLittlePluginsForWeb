d6e716e507ed6e21bc76ee00c62c0da11473600b
KOHINA <283959070@qq.com>
2016-09-21 09:46:53 +0800

    Update GetNum.java

--- getPhoneNumBy360/GetNum.java
+++ getPhoneNumBy360/GetNum.java
@@ -1,14 +1,3 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 public class GetNum {
     public static void main(String[] args) {
         Thread thread1 = new Thread(new Get360PhoneLocation(), "A");
